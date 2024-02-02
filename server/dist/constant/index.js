286dfd63edc0a9685db7c6849f07460ec63bbea4
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2024-02-02 10:12:30 +0700

    Delete dist

--- server/dist/constant/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONST = void 0;
-// Import constant
-const google_drive_1 = require("./google_drive");
-exports.CONST = {
-  GOOGLE_DRIVE: google_drive_1.GOOGLE_DRIVE
-};
--- server/dist/utils/regex/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FILE_URL_RULE = void 0;
-exports.FILE_URL_RULE = /((http|https):\/\/[\w\d_\-.]+\.[\w\d]{2,}([:\d]+)?(\/[\w\d\-._?,'/\\+&%$#=~]*)?)/;
